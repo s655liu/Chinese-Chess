09cd063624fa8d458ef76f875a8cdc92893d0673
Shawn Liu <s655liu@uwaterloo.ca>
2026-05-06 23:27:00 -0700

    remove hardcoded eventlet async_mode

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
 bcrypt.init_app(app)
-socketio = SocketIO(app, cors_allowed_origins="*", async_mode='eventlet')
+socketio = SocketIO(app, cors_allowed_origins="*")
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 
